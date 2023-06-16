1f8acab0b256efcabb274f5abf3a53ad4d71ed4f
ryuzakiakira <ryuzakiakira@protonmail.com>
2023-06-16 11:59:23 +0530

    added calculation functions

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+//performs whichever operation is suited    
+
+function operation(a, operator, b) {
+    if (operator === '+') {
+        return add(a, b);
+    } else if (operator === '-') {
+        return subtract(a, b);
+    } else if (operator === '*') {
+        return multiply(a, b);
+    } else if (operator === '/') {
+        return divide(a, b);
+    };
+};
