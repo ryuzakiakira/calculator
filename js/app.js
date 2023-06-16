7a8250df344f739babaa16f2856b7a7d97a70c97
ryuzakiakira <ryuzakiakira@protonmail.com>
2023-06-16 18:18:25 +0530

    added funcionality to update screen on key press

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// calculator functions
+
 function add(a, b) {
     return a + b;
 };
@@ -17,12 +19,14 @@ function divide(a, b) {
 // variables to store the input
 
 let value1;
+let operationalOperator; 
 let value2;
 
 //performs whichever operation is suited    
 
 function operate(a, operator, b) {
     value1 = a;
+    operationalOperator = operator;
     value2 = b;
     if (operator === '+') {
         return add(a, b);
@@ -35,3 +39,15 @@ function operate(a, operator, b) {
     };
 };
 
+// update screen
+
+function updateScreen(e) {
+    const onScreen = e.target.id;
+    const screen = document.querySelector('.screen-text')
+    screen.textContent = onScreen;
+};
+
+const keys = document.getElementById('calculator-keys')
+keys.addEventListener('click', (event => {
+    updateScreen(event);
+}));
