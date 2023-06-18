ede372f24a9bbcb0481822273bc7e9f2e49e811b
ryuzakiakira <ryuzakiakira@protonmail.com>
2023-06-18 12:46:22 +0530

    done adding functionality to update display and input decimal correctly

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-const availableOperators = ['/', '*', '-', '+']
-
 // calculator functions
 
 function add(a, b) {
@@ -32,3 +30,52 @@ function operate(a, operator, b) {
     };
 };
 
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    const display = document.querySelector('.screen');
+    display.value = calculator.displayValue;
+}
+
+updateDisplay()
+
+const keys = document.querySelector('.keys');
+keys.addEventListener('click', (event) => {
+    const {target} = event;
+    if (!target.matches('button')) return;
+    if(target.classList.contains('operator')) {
+        console.log(target.value)
+        return
+    };
+
+    if(target.classList.contains('decimal')) {
+        inputDecimal(target.value);
+        updateDisplay()
+        return
+    };
+
+    if (target.classList.contains('all-clear')) {
+        console.log(target.value)
+        return
+    };
+
+    inputDigit(target.value);
+    updateDisplay();
+});
+
+
+function inputDigit(digit) {
+    const {displayValue} = calculator
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+};
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    };
+};
